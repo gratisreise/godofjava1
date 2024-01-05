3d0d3b8936efb47e8404ae24461cc9412844c673
noah <jeonghogim318@gmail.com>
2024-01-05 10:26:49 +0900

    ch09:자바를 배우면 패키지와 접근 제어자는 꼭 알아야 해요

---
+++ src/ch09/variable/VariableTypes.java
@@ -0,0 +1,10 @@
+package ch09.variable;
+
+public class VariableTypes {
+    static int classVariable;
+    int instanceVariable;
+
+    public void method(int parameter) {
+        int localVariable;
+    }
+}
